9c843dac7ac7cc35d307bc7b1d102fc25f9937e0
Caio Sanches Bentes <caiosbentes@users.noreply.github.com>
2019-01-24 19:05:09 -0200

    new
    new

---
+++ Oracle-linux7-multipath.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# glpi.sh
+#
+# Autor     : Caio Bentes <caio.bentes@solustecnologia.com.br>
+#
+#  -------------------------------------------------------------
+#   Este programa Configura mapeamento de dispositivo multipath em um servidor Oracle Linux 7.6
+#
